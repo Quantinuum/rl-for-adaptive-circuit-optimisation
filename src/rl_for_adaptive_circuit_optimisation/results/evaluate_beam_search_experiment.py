6570d0c4f6c977457be51f085de893d253908161
Ifan Williams <ifan.williams@quantinuum.com>
2026-03-11 19:56:57 +0000

    refactor plotting

--- src/rl_for_adaptive_circuit_optimisation/results/evaluate_beam_search_experiment.py
+++ src/rl_for_adaptive_circuit_optimisation/results/evaluate_beam_search_experiment.py
@@ -1,126 +1,8 @@
 import argparse
 
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
 
-def plot_reward_result(data: pd.DataFrame) -> plt.Figure:
-    """
-    Plot the cumulative reward results for beam search and other optimisation methods.
-
-    :param data: DataFrame containing the cumulative reward results.
-    :return: Figure object containing the plot.
-    """
-
-    h = 4
-    w = 1.3 * h
-
-    plt.figure(figsize=(w, h))
-
-    sns.set(rc = {'figure.figsize':(w, h)})
-
-    sns.set_theme(style="whitegrid")
-
-    plot = sns.boxplot(
-        data=data,
-        y="Cumulative Reward",
-        hue="Compilation Method",
-        fill=False,
-        showfliers=False,
-        hue_order=[
-            'Model',
-            'QuantinuumDefaultThree',
-            'QuantinuumDefaultTwo',
-            'GreedySearch',
-            'Depth2Width1',
-            'Depth2Width2',
-            'Depth3Width3',
-            'Depth4Width4',
-        ]
-    )
-
-    plot.yaxis.set_minor_locator(
-        ticker.AutoMinorLocator(n=5)
-    )  # 4 minor ticks between major ticks
-    plot.grid(which="minor", linestyle="--", linewidth=0.5, color="gray", alpha=0.5)
-
-    # Manually create the legend
-    legend_handles, _ = plot.get_legend_handles_labels()
-    plt.legend(legend_handles, [
-            'RL Model (This Work)',
-            'QuantinuumDefaultThree',
-            'QuantinuumDefaultTwo',
-            'GreedySearch',
-            'Depth2Width1',
-            'Depth2Width2',
-            'Depth3Width3',
-            'Depth4Width4',
-        ], ncol=2, title='Optimisation Methods', loc='upper left', bbox_to_anchor=(0, 0))
-
-    return plot.get_figure()
-
-
-def plot_timing_result(data: pd.DataFrame) -> plt.Figure:
-    """Plot the timing results for beam search and other optimisation methods.
-
-    :param data: DataFrame containing the timing results.
-    :return: Figure object containing the plot.
-    """
-
-    h = 4
-    w = 1.3 * h
-
-    sns.set_theme(style="whitegrid")
-
-    plot = sns.lmplot(
-        data=data,
-        x="Original 2 Qubit Gate Count",
-        y="Time Taken (s)",
-        hue="Compilation Method",
-        markers=".",
-        height=4,
-        aspect=1.225,
-        lowess=True,
-        scatter_kws={"s": 2},
-        hue_order=[
-            'Model',
-            'QuantinuumDefaultThree',
-            'QuantinuumDefaultTwo',
-            'GreedySearch',
-            'Depth2Width1',
-            'Depth2Width2',
-            'Depth3Width3',
-            'Depth4Width4',
-        ],
-        legend=False
-    )
-
-    legend_handles, _ = plot.axes[0][0].get_legend_handles_labels()
-    lgnd = plt.legend(
-        legend_handles, [
-            'RL Model (This Work)',
-            'QuantinuumDefaultThree',
-            'QuantinuumDefaultTwo',
-            'GreedySearch',
-            'Depth2Width1',
-            'Depth2Width2',
-            'Depth3Width3',
-            'Depth4Width4',
-        ], ncol=2, title='Optimisation Methods', loc='upper left', bbox_to_anchor=(0, -0.15)
-    )
-
-    for handle in lgnd.legend_handles:
-        handle.set_sizes([100])
-
-    plot.axes[0][0].set_xlim(100, 300)
-    plot.set(yscale="log")
-
-    plot.axes[0][0].set(xlabel='Original Two-Qubit Gate Count')
-
-    return plot.figure
+from .plots import plot_reward_result_beam_search, plot_timing_result_beam_search
 
 if __name__ == "__main__":
     
@@ -138,8 +20,8 @@ if __name__ == "__main__":
     with open(args.results_file, "r") as f:
         data = pd.read_csv(f)
 
-    reward_fig = plot_reward_result(data)
+    reward_fig = plot_reward_result_beam_search(data)
     reward_fig.savefig("reward_results.png", bbox_inches="tight")
-    
-    timing_fig = plot_timing_result(data)
+
+    timing_fig = plot_timing_result_beam_search(data)
     timing_fig.savefig("timing_results.png", bbox_inches="tight")
